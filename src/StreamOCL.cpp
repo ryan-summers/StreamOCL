fc7a13c41d0b69852672d7a86b05525c3ca187b6
ryan <summers.ryan.m@gmail.com>
2015-12-23 23:39:56 -0800

    Addition of Copyright

--- src/StreamOCL.cpp
+++ src/StreamOCL.cpp
@@ -1,4 +1,23 @@
 #include "StreamOCL.hpp"
+/*
+    StreamOCL - GNU GPL Copyright Notice
+    Copyright (C) 2015  Ryan Summers
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
 //Default constructor for the OpenCL Class
 OpenCL_Data::OpenCL_Data() 
 {
